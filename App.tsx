6859f0c1c4490e51ef0b1d5f1b766550bd20ca88
Niño Gabriel C. Casanova <gabrielcasanovaq@gmail.com>
2026-03-18 19:13:29 +0800

    Updated Notification 2.0

--- App.tsx
+++ App.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/navigation/AppNavigator";
 import { ThemeProvider } from "./src/context/ThemeContext";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
 
 export default function App() {
   return (
--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,15 +1,5 @@
 import * as Notifications from "expo-notifications";
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: false,
-    shouldShowBanner: true,
-    shouldShowList: true,
-  }),
-});
-
 export const sendNotification = async () => {
   await Notifications.scheduleNotificationAsync({
     content: {
